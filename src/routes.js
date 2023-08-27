28c55e600f7cd81ce3490a95ea726dd489df3f84
Tias <trisn.eclipse@gmail.com>
2023-08-28 00:41:34 +0600

    dashboard preliminary phase 2

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 import { IoGridOutline, IoHomeOutline } from "react-icons/io5";
 import { BsSpeedometer2 } from "react-icons/bs";
-import { BiUserCircle } from "react-icons/bi";
-import { AiOutlineBarChart } from "react-icons/ai";
+import { BiUserCircle,BiCategory,BiSolidLogOutCircle } from "react-icons/bi";
+import { AiOutlineBarChart,AiFillTags } from "react-icons/ai";
+import { VscBook } from "react-icons/vsc";
+import { MdLocalLibrary } from "react-icons/md";
+import { GiMeal } from "react-icons/gi";
 
 export default [
     {
@@ -15,18 +18,43 @@ export default [
         Icon: BiUserCircle
     },
     {
+        to: '/recipes',
+        name: 'Recipes',
+        Icon: VscBook
+    },
+    {
+        to: '/categories',
+        name: 'Category',
+        Icon: BiCategory
+    },
+    {
+        to: '/tags',
+        name: 'Tags',
+        Icon: AiFillTags
+    },
+    {
+        to: '/recipefeed',
+        name: 'Recipe Feed',
+        Icon: MdLocalLibrary
+    },
+    {
+        to: '/mealplan',
+        name: 'Meal Plans',
+        Icon: GiMeal
+    },
+    {
         to: '/dashboard',
         name: 'Dashboard',
         Icon: BsSpeedometer2
     },
     {
-        to: '/test-components',
-        name: 'Test Components',
-        Icon: IoGridOutline
-    },
-    {
         to: '/statistics',
         name: 'Statistics',
         Icon: AiOutlineBarChart
+    },
+    {
+        to: '/home',
+        name: 'Log Out',
+        Icon: BiSolidLogOutCircle
     }
 ];
