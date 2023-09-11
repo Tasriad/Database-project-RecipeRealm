dfa89a93756fe90e9e00fad69ed52dcc97e788db
hasnaenadil <2005115@ugrad.cse.buet.ac.bd>
2023-09-11 08:58:41 +0600

    near final v1

---
+++ src/app/api/remove_fav/route.js
@@ -0,0 +1,24 @@
+import { NextResponse, NextRequest } from 'next/server'
+import runQuery from '@/utils/database_manager';
+import oracledb from 'oracledb';
+import jwt from 'jsonwebtoken';
+
+export async function POST(request) {
+    try {
+        const reqBody = await request.json()
+        const { recipe_id } = reqBody;
+        const token = request.cookies.get('current_user')?.value || ''
+        const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+        const user_id = decoded.id;
+        const query = `DELETE FROM favorite WHERE user_id = :user_id AND recipe_id = :recipe_id`
+        const binds = {
+            user_id: { dir: oracledb.BIND_IN, type: oracledb.NUMBER, val: user_id },
+            recipe_id: { dir: oracledb.BIND_IN, type: oracledb.NUMBER, val: recipe_id }
+        }
+        console.log(binds)
+        const result = await runQuery(query, true, binds);
+        return NextResponse.json({ message: 'success', success: true }, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 200 })
+    }
+}
---
+++ src/utils/connection_pool.js
@@ -0,0 +1,20 @@
+import { getConnection } from 'oracledb';
+import { createPool } from 'generic-pool';
+
+const pool = createPool({
+    create: async () => {
+        const connection = await getConnection({
+            user: "PROJECT",
+            password: "12345",
+            connectString: "localhost:1521/orclpdb",
+        });
+        console.log('connection created');
+        return connection;
+    },
+    destroy: async (connection) => {
+        await connection.close();
+        console.log('connection closed');
+    }
+});
+
+export default pool;
